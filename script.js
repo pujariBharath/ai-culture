fce24ba7e5e5516438eea28771d40f6f2a116b6e
Bharath <132049800+pujariBharath@users.noreply.github.com>
2024-11-13 10:41:20 +0530

    login page

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 });
+
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
